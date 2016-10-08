ab5d2d55a0aee0b92e0a8104c8d1708c8b1fac99
Simone Basso <bassosimone@gmail.com>
2016-10-08 14:37:22 +0200

    nettest/loader.py: let Python expand variables

--- neubot/nettest/loader.py
+++ neubot/nettest/loader.py
@@ -8,26 +8,17 @@
 """ Net test loader """
 
 import logging
+import os
 import re
 import shlex
 import sys
 
 def load(spec, params):
     """ Given a specific test name and spec, load command line arguments """
-    command_line = []
-    for elem in shlex.split(spec["command_line"]):
-        if not elem.startswith("$"):
-            command_line.append(elem)
-            continue
-        if elem == "$python":
-            command_line.append(sys.executable)
-            continue
-        variable_name = elem[1:]  # Remove the leading `$`
-        if variable_name not in params:
-            # TODO: add possibility of specifying the default value
-            logging.warning("No mapping for: %s", variable_name)
-            return
-        value = params[variable_name]
+
+    # TODO: add the possibility of specifying the default value
+
+    for variable_name in params:
         if variable_name not in spec["params"]:
             logging.warning("No descriptor for: %s", variable_name)
             return
@@ -35,8 +26,17 @@ def load(spec, params):
             logging.warning("No regexp for: %s", variable_name)
             return
         regexp = spec["params"][variable_name]["regexp"]
-        if not re.match(regexp, value):
+        if not re.match(regexp, params[variable_name]):
             logging.warning("Regexp does not match for: %s", variable_name)
             return
-        command_line.append(value)
+
+    params.update({
+        "pwd": os.path.abspath("."),
+        "python": sys.executable
+    })
+
+    command_line = spec["command_line"].format(**params)
+    command_line = shlex.split(command_line)
+    logging.debug("command line: %s", command_line)
+
     return command_line
